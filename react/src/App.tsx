e0aaf75e291e914eb3645cee9527c0ff26e855dc
mathieu <mathieu.minguez@gmail.com>
2024-07-22 18:45:21 +0200

    Init react project + tailwind + shadcn

---
+++ react/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Hero from '@/views/Hero'
+import Navbar from '@/components/navbar'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <Navbar />
+
+      <Hero/>
+
+    </div>
+  )
+}
+
+export default App
